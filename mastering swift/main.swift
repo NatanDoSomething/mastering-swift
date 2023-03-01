06419cf7a9ded89f6bd8aca81000acf06b821239
Natan <natandeveloper@icloud.com>
2023-03-01 21:33:32 +0000

    pattern matching

--- mastering swift/main.swift
+++ mastering swift/main.swift
@@ -1,11 +1,35 @@
-//
-//  main.swift
-//  mastering swift
-//
-//  Created by Natan Gluszko on 01/03/2023.
-//
-
 import Foundation
 
-print("Hello, World!")
+
+    // PATTERN MATCHING
+let name = "natan"
+let dog = "kenzo"
+let teamA = ("natan", "kenzo")
+
+switch name {
+case "natan":
+    print("hay \(name)")
+case "geno":
+    print("your not natan?")
+default:
+    print("how did we come here")
+}
+
+switch (name, dog) {
+case ("natan", "kenzo"):
+    print("hay \(name) and \(dog)")
+case ("geno", "daisy"):
+    print("your not natan?")
+default:
+    print("how did we come here")
+}
+
+switch teamA {
+case ("natan", "kenzo"):
+    print("hay \(name) and \(dog)")
+case ("sdfds", "fsdfsdfds"):
+    print("your not natan?")
+default:
+    print("how did we come here")
+}
 
